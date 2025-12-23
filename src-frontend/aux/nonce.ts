9cf3d622ed80027f9bb27e990863798a5cdd21cd
brweisz <bweisz@eryxsoluciones.com.ar>
2025-12-23 12:15:59 -0300

    Uses a custom poseidon hash for the bls12381 curve, uses it to compute nonce and removes fixed length of the nonce

--- src-frontend/aux/nonce.ts
+++ src-frontend/aux/nonce.ts
@@ -3,6 +3,7 @@ import {poseidonHash} from "@mysten/zklogin";
 import {toBigEndianBytes} from "@mysten/sui.js/zklogin";
 import {base64url} from "jose";
 import {toHEX} from "@mysten/bcs";
+import { poseidon3 } from "poseidon-bls12381";
 
 const NONCE_LENGTH = 27;
 
@@ -19,12 +20,12 @@ export function generateNonce(publicKey: PublicKey, maxEpoch: number, randomness
     const publicKeyBytes = toBigIntBE(publicKey.toRawBytes());
     // const eph_public_key_0 = publicKeyBytes / 2n ** 128n;
     // const eph_public_key_1 = publicKeyBytes % 2n ** 128n;
-    const bigNum = poseidonHash([publicKeyBytes, maxEpoch, BigInt(randomness)]);
+    const bigNum = poseidon3([publicKeyBytes, BigInt(maxEpoch), BigInt(randomness)]);
     // const bigNum = poseidonHash([eph_public_key_0, eph_public_key_1, maxEpoch, BigInt(randomness)]);
-    const Z = toBigEndianBytes(bigNum, 20);
-    const nonce = base64url.encode(Z);
-    if (nonce.length !== NONCE_LENGTH) {
+    // const Z = toBigEndianBytes(bigNum, 20);
+    const nonce = base64url.encode(bigNum.toString());
+    /*if (nonce.length !== NONCE_LENGTH) {
         throw new Error(`Length of nonce ${nonce} (${nonce.length}) is not equal to ${NONCE_LENGTH}`);
-    }
+    }*/
     return nonce;
 }
